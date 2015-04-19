2bc73d8370860508df396a12425eb7f34a105805
Quildreen Motta <quildreen@gmail.com>
2015-04-19 14:44:11 -0300

    Fixes copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Text.PrettyPrinting'
-copyright = u'2015, Quildreen Motta'
+copyright = u'Quildreen Motta'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
